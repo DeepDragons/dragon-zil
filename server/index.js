b34747aef310c1562b67c124782ef2b88a29ef32
Rinat <lich666black@gmail.com>
2020-11-02 16:46:22 +0500

    added static asdefaul for server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -11,6 +12,8 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 // middleware for json body parsing
 app.use(bodyParser.json({ limit: '1mb' }));
+
+app.use(express.static(path.join(__dirname, '../', './dist')));
 
 app.use(signRouter);
 
