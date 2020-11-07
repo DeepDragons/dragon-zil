6901cc1ef4be37e8a9534aee6e79298f2c5c2332
Rinat <lich666black@gmail.com>
2020-11-07 15:43:33 +0500

    added radar mixin

---
+++ src/mixins/radar.js
@@ -0,0 +1,69 @@
+import Chart from 'chart.js'
+
+var defaultOptions = {
+  legend: {
+    display: true
+  },
+  scale:{
+    pointLabels:{
+       fontColor:"#d528d0",
+    }
+  }
+}
+
+
+export default {
+  data() {
+    return {
+      attacName: 'A',
+      defName: 'D',
+      radarChartData: {
+        labels: [],
+        datasets: []
+      }
+    }
+  },
+  computed: {
+    __combatGensLabels() {
+      let amountAttackGens = 14
+      let amountAllGens = 30
+      let array = []
+
+      for (let index = 0; index < amountAllGens; index++) {
+        if (index <= amountAttackGens) {
+          array.push(`${this.attacName} ${index}`)
+        } else {
+          array.push(`${this.defName} ${index}`)
+        }
+      }
+
+      return array
+    }
+  },
+  mounted() {
+    this.radarChartData.labels = this.__combatGensLabels
+  },
+  methods: {
+    __generateCharts(ctx, options=defaultOptions) {
+      let combatChart = new Chart(ctx, {
+        type: 'radar',
+        data: this.radarChartData,
+        options
+      })
+
+      return combatChart
+    },
+    __parseGens(id, gensArray, label, borderColor,
+      pointHoverBackgroundColor, borderWidth=2) {
+      let dataset = {
+        label: `${label} #${id}`,
+        borderColor: borderColor,
+        pointHoverBackgroundColor: pointHoverBackgroundColor,
+        data: gensArray,
+        borderWidth: borderWidth
+      }
+
+      return dataset
+    }
+  }
+}
---
+++ src/page/Dragon.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Card />
+  </div>
+</template>
+
+<script>
+import Card from '@/components/Card'
+import RadarMixin from '@/mixins/radar'
+
+export default {
+  name: 'Dragon',
+  mixins: [RadarMixin],
+  components: {
+    Card
+  },
+  mounted() {
+  }
+}
+</script>
+
+<style>
+
+</style>
